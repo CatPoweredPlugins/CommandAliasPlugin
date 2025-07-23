961470044aca27036b6030156bbfbd1bd5dda313
Rudokhvist <kotlyar.andrey@gmail.com>
2025-07-23 18:27:17 +0300

    fix null check

--- CommandAliasPlugin/CommandAliasPlugin.cs
+++ CommandAliasPlugin/CommandAliasPlugin.cs
@@ -98,7 +98,7 @@ internal sealed class CommandAliasPlugin : IGitHubPluginUpdates, IBotCommand2, I
 			return null;
 		}
 		if (args[0].Equals("COMMANDS", StringComparison.OrdinalIgnoreCase)) {
-		   HashSet<string> aliases = [.. Aliases.Where(alias => !string.IsNullOrEmpty(alias.AliasName)).Select(alias => alias.AliasName.ToUpperInvariant())];
+		   HashSet<string> aliases = [.. Aliases.Where(alias => !string.IsNullOrEmpty(alias?.AliasName)).Select(alias => alias.AliasName.ToUpperInvariant())];
 		   return "\n" + Name + "\n\n" + string.Join("\n", aliases) + "\n---------------------";
 		}
 		List<Alias> foundAliases = [.. Aliases.Where(alias => !string.IsNullOrEmpty(alias.AliasName) && alias.AliasName.Equals(args[0], StringComparison.OrdinalIgnoreCase) && alias.ParamNumber <= (args.Length - 1)).OrderByDescending(alias => alias.ParamNumber)];
